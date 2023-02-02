b872a6a2ce1ec4ffbca5dc08b3b3d9c1cb22991b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-02-01 18:41:09 -0600

    Fix typo locations link in side vertical navbar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,7 +15,7 @@ export default {
 					{ text: 'Introduction', link: '/guide/application-api/' },
 					{ text: 'Users', link: '/guide/application-api/users' },
 					{ text: 'Servers', link: '/guide/application-api/servers' },
-					{ text: 'Nodes', link: '/guide/application-api/locations' },
+					{ text: 'Locations', link: '/guide/application-api/locations' },
 					{ text: 'Nodes', link: '/guide/application-api/nodes' },
 					{ text: 'Templates', link: '/guide/application-api/templates' },
 					{ text: 'Addresses', link: '/guide/application-api/addresses' },
